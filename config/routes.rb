986278c8412ec70b06ffe624f21e8cb97a0dad49
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-15 14:54:10 +0200

    got all the stuff working sans the optionals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks' => 'tasks#index'
+  get 'tasks/new' => 'tasks#new'
+  get '/tasks/:id' => 'tasks#show', as: :task
+  post 'tasks' => 'tasks#create'
+  get '/tasks/:id/edit' => 'tasks#edit', as: :edit
+  patch '/tasks/:id/' => 'tasks#update'
+  delete "tasks/:id" => "tasks#destroy"
+
+  # delete 'tasks/:id' => 'tasks#destroy'
 end
